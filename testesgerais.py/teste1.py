17f7dcbc35e1641313066d4eb27d9195e8b73b62
davi neves caetano <davineves.c25@gmail.com>
2026-05-30 20:41:40 +0000

    final do dia 30/05/26

--- testesgerais.py/teste1.py
+++ testesgerais.py/teste1.py
@@ -16,8 +16,17 @@ quantidade_linhas = 5
 quantidade_total_blocos = quantidade_blocos_linha * quantidade_linhas
 
 def criar_blocos(quantidade_blocos_linha, quantidade_linhas):
+    altura_tela = tamanho_tela[1]
+    largura_tela = tamanho_tela[0]
+    distancia_blocos = 5
+    largura_bloco = (largura_tela // quantidade_blocos_linha) - distancia_blocos
+    altura_bloco = 20
     blocos = []
     #criar blocos
+    for j in range(quantidade_linhas):
+        for i in range(quantidade_blocos_linha):
+            bloco = pygame.Rect(i*(largura_bloco + distancia_blocos), j*(altura_bloco + distancia_blocos*2), largura_bloco, altura_bloco)
+            blocos.append(bloco)
     return blocos
 
 cores = {
@@ -35,16 +44,57 @@ pontuacao = 0
 movimento_bola = [1, 1]
 
 #criar as funçoes
+def movimentar_jogador(evento):
+    if evento.type == pygame.KEYDOWN:
+        if evento.key == pygame.K_RIGHT and jogador.right < tamanho_tela[0]:
+            jogador.x += 5
+        if evento.key == pygame.K_LEFT and jogador.left > 0:
+            jogador.x -= 5 
+
+def movimentar_bola(bola):
+    movimento = movimento_bola
+    bola.x += movimento[0]
+    bola.y += movimento[1]
+    if bola.left <= 0 or bola.right >= tamanho_tela[0]:
+        movimento[0] *= -1
+    if bola.top <= 0:
+        movimento[1] *= -1
+    if bola.bottom >= tamanho_tela[1]:
+        fim_jogo = True
+
+    if jogador.colliderect(bola):
+        movimento[1] *= -1 
+    for bloco in blocos:
+        if bloco.colliderect(bola):
+            movimento[1] *= -1
+            blocos.remove(bloco)
+            pontuacao += 1
+    return movimento
 
 #desenhar o que aparecerá na tela
-tela.fill(cores["preto"])
+def desenhar_inicio_jogo(): 
+    tela.fill(cores["preto"])
+    pygame.draw.rect(tela, cores["azul"], jogador)
+    pygame.draw.rect(tela, cores["branco"], bola)
+def desenhar_blocos(blocos):
+    for bloco in blocos:
+        pygame.draw.rect(tela, cores["amarelo"], bloco)
+
+    
+
+blocos = criar_blocos(quantidade_blocos_linha, quantidade_linhas)
+
 
 #cria um loop infinito
 while not fim_jogo:
+    desenhar_inicio_jogo()
+    desenhar_blocos(blocos)
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             fim_jogo = True
-    pygame.time.wait(1)
+    movimentar_jogador(evento)
+    movimento_bola = movimentar_bola(bola)
+    pygame.time.wait(5)
     pygame.display.flip()
 
 pygame.quit()
