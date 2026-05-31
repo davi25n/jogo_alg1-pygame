e0a31649bf58fa4c741a71d0ff8092c9653ec53e
davi neves caetano <davineves.c25@gmail.com>
2026-05-31 18:07:53 +0000

    finalização do teste1 do davi

--- testesgerais.py/teste1.py
+++ testesgerais.py/teste1.py
@@ -37,11 +37,14 @@ cores = {
     "verde": (0, 255, 0)
 }
 
+# Variável GLOBAL que controla o loop do jogo
+# Quando fim_jogo = True, o loop while encerra
 fim_jogo = False
 
 pontuacao = 0
 
-movimento_bola = [1, 1]
+
+movimento_bola = [2, -2]
 
 #criar as funçoes
 def movimentar_jogador(evento):
@@ -55,21 +58,27 @@ def movimentar_bola(bola):
     movimento = movimento_bola
     bola.x += movimento[0]
     bola.y += movimento[1]
-    if bola.left <= 0 or bola.right >= tamanho_tela[0]:
+    if bola.x  <= 0 or bola.x + tamanho_bola >= tamanho_tela[0]:
         movimento[0] *= -1
-    if bola.top <= 0:
+    if bola.y <= 0:
         movimento[1] *= -1
-    if bola.bottom >= tamanho_tela[1]:
-        fim_jogo = True
-
-    if jogador.colliderect(bola):
-        movimento[1] *= -1 
+    if bola.y + tamanho_bola >= tamanho_tela[1]:
+        movimento = None
+    if jogador.collidepoint(bola.x, bola.y):
+        movimento[1] *= -1
     for bloco in blocos:
-        if bloco.colliderect(bola):
+        if bloco.collidepoint(bola.x, bola.y):
             movimento[1] *= -1
             blocos.remove(bloco)
-            pontuacao += 1
     return movimento
+
+def atualizar_pontuacao(pontuacao):
+    fonte = pygame.font.Font(None, 30)
+    texto = fonte.render(f"Pontuação: {pontuacao}", 1, cores["verde"])
+    tela.blit(texto, (10, 150))
+    if pontuacao >= quantidade_total_blocos:
+        return True
+    return False
 
 #desenhar o que aparecerá na tela
 def desenhar_inicio_jogo(): 
@@ -89,12 +98,15 @@ blocos = criar_blocos(quantidade_blocos_linha, quantidade_linhas)
 while not fim_jogo:
     desenhar_inicio_jogo()
     desenhar_blocos(blocos)
+    fim_jogo = atualizar_pontuacao(quantidade_total_blocos - len(blocos))
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             fim_jogo = True
     movimentar_jogador(evento)
     movimento_bola = movimentar_bola(bola)
-    pygame.time.wait(5)
+    if not movimento_bola:
+        fim_jogo = True
+    pygame.time.wait(3)
     pygame.display.flip()
-
+print("game over")
 pygame.quit()
